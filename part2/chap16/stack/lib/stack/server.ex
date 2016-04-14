8b37dab28a3e494fa37c4224c5d9e72c9895239d
Pierre PIRONIN <pierre@pierrepironin.fr>
2016-04-14 19:18:48 +0200

    Extend stack with push

--- part2/chap16/stack/lib/stack/server.ex
+++ part2/chap16/stack/lib/stack/server.ex
@@ -5,4 +5,8 @@ defmodule Stack.Server do
   def handle_call(:pop, _from, [head | tail]) do
     {:reply, head, tail}
   end
+  
+  def handle_cast({:push, element}, stack) do
+    {:noreply, [element | stack]}
+  end
 end
