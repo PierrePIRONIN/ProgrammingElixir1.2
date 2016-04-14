01ca6f8ee30861d50cf4e66471a5925c8c09ff69
Pierre PIRONIN <pierre@pierrepironin.fr>
2016-04-14 19:11:47 +0200

    Extend sequence server

--- part2/chap16/sequence/lib/sequence/server.ex
+++ part2/chap16/sequence/lib/sequence/server.ex
@@ -10,4 +10,11 @@ defmodule Sequence.Server do
     {:reply, new_number, new_number}
   end
 
+  def handle_cast({:increment_number, delta}, current_number) do
+    {:noreply, current_number + delta}
+  end
+  
+  def format_status(_reason, [ _pdict, state ]) do
+    [data: [{'State', "My current state is '#{inspect state}', and I'm happy"}]]
+  end
 end
