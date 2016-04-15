419a85bb3c3b1e9fd25faa72fe2238c172cc16ba
Pierre PIRONIN <pierre@pierrepironin.fr>
2016-04-15 08:27:36 +0200

    Extend stack with external API

--- part2/chap16/stack/lib/stack/server.ex
+++ part2/chap16/stack/lib/stack/server.ex
@@ -1,11 +1,33 @@
 defmodule Stack.Server do
 
   use GenServer
+  
+  #####
+  # External API
+  
+  def start_link(stack) do
+    GenServer.start_link(__MODULE__, stack, name: __MODULE__)
+  end
+  
+  def pop do
+    GenServer.call(__MODULE__, :pop)
+  end
+  
+  def push(element) do
+    GenServer.cast(__MODULE__, {:push, element})
+  end
+  
+  #####
+  # GenServer implementation
 
+  def handle_call(:pop, _from, []) do
+    raise RuntimeError, message: "Can not pop a empty list"
+  end
+  
   def handle_call(:pop, _from, [head | tail]) do
     {:reply, head, tail}
   end
-  
+    
   def handle_cast({:push, element}, stack) do
     {:noreply, [element | stack]}
   end
